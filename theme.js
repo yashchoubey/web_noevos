67e06c157962f591c38fbc8b64272b962225d655
yashchoubey <yashchoubey2009@gmail.cm>
2017-07-21 13:08:50 +0530

    initial commit

---
+++ theme.js
@@ -0,0 +1,6 @@
+angular.module('myApp', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('pink')
+    .accentPalette('orange');
+});
